a4e8cdd7eaa551e6ba93ee4ca04c20f7361d06a3
patrickreinan <patrickreinan@gmail.com>
2022-06-18 20:00:57 -0300

    tls-generate

--- tls-generate.sh
+++ tls-generate.sh
@@ -17,6 +17,10 @@ CN=
 
 loadDefaults() {
 
+    if [ -z $DOMAIN ]
+    then
+        DOMAIN="localhost"
+    fi
     
     if [ -z $KEY ]
     then
@@ -43,10 +47,6 @@ loadDefaults() {
         COUNTRY=BR
     fi
 
-    if [ -z $CN ] 
-    then
-        CN=$DOMAIN
-    fi
 
     destination="./tls"
     domainRoot="$destination/$DOMAIN"
@@ -58,13 +58,6 @@ loadDefaults() {
 
 }
 
-checkArgs() {
-    if [ -z $DOMAIN ]
-    then
-        echo "Invalid DOMAIN"
-        exit 1
-    fi
-}
 
 prepareDestination() {
     rm -rf $domainRoot
@@ -75,10 +68,8 @@ prepareDestination() {
 
 generateConfig() {
  
-    
-   
     cat tls-generate.cnf \
-    | sed  "s/%CN/$CN/g" \
+    | sed  "s/%CN/$1/g" \
     | sed "s/%COUNTRY/$COUNTRY/g" \
     | sed "s/%STATE/$STATE/g" \
     | sed "s/%CITY/$CITY/g" \
@@ -87,14 +78,19 @@ generateConfig() {
 }
 generateCert() {
 
+    generateConfig default-ca
+
     if [ ! -f "$caKeyPath" ] || [ ! -f "$caPEMPath" ] 
     then
         openssl genrsa -des3  -passout pass:$KEY -out $caKeyPath 2048
         openssl req -x509 -new -nodes -key $caKeyPath -sha256 -days $DAYS -out $caPEMPath -config $cnfPath -batch -passin pass:$KEY
     fi
 
+    generateConfig $DOMAIN
+
     openssl genrsa -out $domainPath.key 2048
     openssl req -new -key $domainPath.key -out $domainPath.csr -config $cnfPath -batch 
+
 
     cat > $domainPath.ext \
      << EOF
@@ -103,21 +99,25 @@ generateCert() {
         keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
         subjectAltName = @alt_names
         [alt_names]
-        DNS.1 = $domainPath
+        DNS.1 = $DOMAIN
 EOF
     
     openssl x509 -req -in $domainPath.csr -CA $caPEMPath -CAkey $caKeyPath \
     -CAcreateserial -CAserial $caSerial -out $domainPath.crt -days $DAYS -sha256 -extfile $domainPath.ext  -passin pass:$KEY 
+   
+    openssl pkcs12 -inkey $domainPath.key -in $domainPath.crt -export -out $domainPath.pfx  -passout pass:$KEY 
+
+    #cat $domainPath.crt $caPEMPath >> $domainPath-bundle.crt
 }
 
 cleanup() {
     rm -rf $cnfPath
 }
 
-clear
-checkArgs 
+clear 
 loadDefaults
 prepareDestination
-generateConfig
 generateCert 
 cleanup
+
+
